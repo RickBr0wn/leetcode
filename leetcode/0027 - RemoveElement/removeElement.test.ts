4f22e21bb26142257ece1613cdd2abe45073c0f0
Rick Brown <ricky.brown.00@gmail.com>
2023-03-03 11:16:41 +0000

    add removeElement

---
+++ leetcode/0027 - RemoveElement/removeElement.test.ts
@@ -0,0 +1,9 @@
+// import removeElement from './removeElement'
+
+describe('removeElement (fn)', () => {
+  it('should pass all basic tests', () => {
+    // expect(removeElement([3, 2, 2, 3], 3)).toBe(2)
+    // expect(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2).toBe(5)
+    expect(true).toBe(true)
+  })
+})
